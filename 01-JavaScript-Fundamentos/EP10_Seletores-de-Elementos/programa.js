c184a7382ba600ac1dae1280e2724c77537da8b0
Leonardo Laranjeira <137966224+leonardolaranjeiradev@users.noreply.github.com>
2026-07-24 15:03:31 -0300

    EP10_Seletores-de-Elementos

---
+++ 01-JavaScript-Fundamentos/EP10_Seletores-de-Elementos/programa.js
@@ -0,0 +1,20 @@
+// document.getElementById
+console.log(document.getElementById("meu-formulario"));
+
+// document.getElementsByTagName
+console.log(document.getElementsByTagName("li"));
+
+// document.getElementsByClassName
+console.log(document.getElementsByClassName("item"));
+
+// document.querySelector
+console.log(document.querySelector("li"));
+console.log(document.querySelector("#meu-formulario"));
+console.log(document.querySelector(".item"));
+
+// document.querySelectorAll
+console.log(document.querySelectorAll(".item"));
+
+
+
+
